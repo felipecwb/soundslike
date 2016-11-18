08ed29f4ed0a2fd8cbb8b230b7b98cf235eb907a
Felipe Francisco <felipefrancisco.cwb@mail.com>
2016-11-18 15:56:04 -0200

    WIP Tests

---
+++ tests/TextSearchTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Felipecwb\SoundsLike\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class TextSearch extends TestCase
+{
+    /**
+     * @test
+     */
+    public function search()
+    {
+        //TODO
+    }
+}
